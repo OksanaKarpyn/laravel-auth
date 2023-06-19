4c531c10569d065dd8528f0eb3ef1be450701ecd
Oksana Karpyn <oksana101992@gmail.com>
2023-06-19 21:15:29 +0200

    model,sider,controll.migrate,seeder stamp in pag

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
+<div class="container my-5">
+    {{-- <h2 class="fs-4 text-secondary my-4">
         {{ __('Dashboard') }}
     </h2>
     <div class="row justify-content-center">
@@ -20,7 +20,32 @@
                     {{ __('You are logged in!') }}
                 </div>
             </div>
+        </div> 
+    </div>--}}
+    <h1 class="text-center">I MIEI PROGETTI</h1>
+</div>
+<div class="container my-4">
+    <div class="row">
+       
+        @forelse ($projects as $elem)
+        <div class="col-4 d-flex">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">{{$elem['title']}}</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">{{$elem['type']}}</h6>
+              <p class="card-text">{{$elem['content']}}</p>
+              <p class="card-text">{{$elem['price']}}</p>
+    
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+            </div>
+          </div>
         </div>
+        @empty
+            <h2>non ci sono elementi</h2>
+        @endforelse   
+       
     </div>
+    
 </div>
 @endsection
